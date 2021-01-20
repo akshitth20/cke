1478dc2096197dcef7a7d51a316127329ae856ad
akshitth20 <67254118+akshitth20@users.noreply.github.com>
2021-01-20 19:02:21 +0530

    Add files via upload
    1.8. Write a program to input a number and print corresponding character

---
+++ 1.8.c
@@ -0,0 +1,15 @@
+
+/*
+1.8. Write a program to input a number and print corresponding character*/
+#include<stdio.h>
+ main(){
+unsigned num;
+printf("enter the number should be between 64 to 127\n");
+scanf("%u",&num);
+printf("%c",num);
+}
+/*output
+enter the number should be between 64 to 127
+68
+D
+*/
