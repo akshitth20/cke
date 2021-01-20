52e389489febf7ff5af0c0e0a69103c3fa38c5f8
akshitth20 <67254118+akshitth20@users.noreply.github.com>
2021-01-20 18:56:20 +0530

    Add files via upload
    1.4. Write a program input a decimal number and print into decimal, octal and hexa decimal

---
+++ 1.4.c
@@ -0,0 +1,15 @@
+/*1.4. Write a program input a decimal number and print into decimal, octal and hexa decimal*/
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf (("enter a decimal no"));
+    scanf("%d",&n);
+    printf("decimal no=%d\noctal no=%o\n hexadecimal no =%x",n,n,n);
+}
+/*output
+enter a decimal no24
+decimal no=24
+octal no=30
+ hexadecimal no =18
+*/
