bd7b1d5678e20349e0bbd5c8391ca5ec1878ec5b
akshitth20 <67254118+akshitth20@users.noreply.github.com>
2021-01-20 18:52:32 +0530

    Add files via upload

---
+++ 1.1.c
@@ -0,0 +1,17 @@
+/*
+1.1. Write a program to input a floating number and show that number with two decimal points
+
+*/
+#include<stdio.h>
+void main()
+{
+    float n;
+    printf("Enter the no");
+    scanf("%f",&n);
+    printf("the number is %.0f",n);
+
+}
+/*output
+Enter the no24
+the number is 24
+*/
