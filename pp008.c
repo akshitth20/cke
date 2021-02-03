1f19020d9f8d2e3f5335da41578e603cb77b6510
akshitth20 <67254118+akshitth20@users.noreply.github.com>
2021-02-03 13:29:51 +0530

    Update pp008.c

--- pp008.c
+++ pp008.c
@@ -1,14 +1,12 @@
+/*pp008: Write a program to input few numbers from command line and print all even numbers from given set of numbers.*/
 #include<stdio.h>
-struct Test{
-unsigned n:1;
-};
-main(){
-int num;
-struct Test t;
-system("cls"); printf("Enter a number:");scanf("%d",&num);
-t.n=num;
-if(t.n)
-    printf("odd");
-    else printf("even");
-    getch();
+main(int argc,char*argv[]){
+    if(argc==1)
+    {
+        printf("Synatx is: evenodd<number>");
+        exit(1);
+    }
+    int num=atoi(argv[1]);
+    num%2?printf("%d is odd",num):printf("%d is even",num);
 }
+
