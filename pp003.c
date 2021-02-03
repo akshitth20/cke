baeecb5c9c2ae08b12f31a36dfb2759ead6b19a8
akshitth20 <67254118+akshitth20@users.noreply.github.com>
2021-02-03 13:13:44 +0530

    Update pp003.c

--- pp003.c
+++ pp003.c
@@ -1,4 +1,4 @@
-//using bitwise AND. check for even and odd no
+/*pp003: Write a program to input a number and check it to be even or odd using bitwise operator.*/
 #include<stdio.h>
 void main()
 {
@@ -8,3 +8,9 @@ void main()
     if((n&1)==0)
 printf("No is even");else printf("No is odd");
 }
+/*output
+enter a number5
+No is odd
+enter a number16
+No is even
+*/
