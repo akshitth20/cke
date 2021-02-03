e273b8a8bfa33a670d36555aab946a0f52b30ea3
akshitth20 <67254118+akshitth20@users.noreply.github.com>
2021-02-03 13:20:07 +0530

    Update pp005.c

--- pp005.c
+++ pp005.c
@@ -1,7 +1,8 @@
+/*pp005: Write a program to input two numbers and print all even numbers in given range.*/
 #include<stdio.h>
 void main()
 {
-    int n,i,f1,f2,rem;
+    int i,f1,f2,rem;
     printf("enter the starting no"); scanf("%d",&f1);
     printf("enter the ending no"); scanf("%d",&f2);
     printf("\n The even no b/w %d and %d are ",f1,f2);
@@ -14,3 +15,19 @@ void main()
 
         }
 }
+/*output
+enter the starting no2
+enter the ending no20
+
+ The even no b/w 2 and 20 are
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+*/
